9b27e6164cbe9d3eaff5dad28c31847dd99bc7c8
vasukiraj01 <73832102+vasukiraj01@users.noreply.github.com>
2022-07-21 17:44:36 +0530

    Add files via upload

---
+++ Protectedspecifier2.java
@@ -0,0 +1,15 @@
+package simplelearn1;
+
+import simplelearn.*;
+//protected specifier using another package
+
+public class Protectedspecifier2 extends Protectedspecifier {
+	
+
+public static void main(String[] args) {
+	
+	Protectedspecifier2 obj = new Protectedspecifier2();
+	obj.Animal();
+
+}
+}
