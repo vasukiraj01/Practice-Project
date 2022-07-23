72505f9f83fe6f5bf4c7e7d4391cf6952096513a
vasukiraj01 <73832102+vasukiraj01@users.noreply.github.com>
2022-07-23 11:50:45 +0530

    Add files via upload

---
+++ Practice-Projects 2/Dimondex.java
@@ -0,0 +1,30 @@
+package Thread_Exception;
+
+interface First 
+{  
+    default void show() 
+    { 
+        System.out.println("Default First"); 
+    } 
+} 
+interface Second 
+{  
+    default void show() 
+    { 
+        System.out.println("Default Second"); 
+    } 
+}  
+public class Dimondex implements First, Second 
+{  
+    public void show() 
+    {  
+        First.super.show(); 
+        Second.super.show(); 
+    } 
+    public static void main(String args[]) 
+    { 
+        Dimondex ob = new Dimondex(); 
+        ob.show(); 
+    } 
+}
+
