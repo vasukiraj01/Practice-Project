a39a07d21f44219f2aa4742a5f679ea4e746db08
vasukiraj01 <73832102+vasukiraj01@users.noreply.github.com>
2022-08-30 19:26:43 +0530

    spring security

---
+++ Practice Projects_Phase3/DynamicTesting/src/pkg8/DynamicTesting.java
@@ -0,0 +1,31 @@
+package pkg8;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+
+class DynamicTesting {
+	/* Dynamic testing menas test case generate at runtime
+		@TestFactory-- using for DynamicTest. it always return stream, collection, iterator, Junitexception
+		it should  be used with static or privae methods
+		@BeforeEach,@AfterEach will not call with DynamicTest*/
+		@TestFactory
+		Collection<DynamicTest> dynamicTestWithCollection(){
+			return  Arrays.asList(
+					
+					
+					
+					DynamicTest.dynamicTest("Add Test", ()->assertEquals(2,Math.addExact(5, 6))),
+					
+					
+					
+					DynamicTest.dynamicTest("Multiply Test", ()->assertEquals(4,Math.multiplyExact(2, 2)))
+					);
+					
+		}
+}
